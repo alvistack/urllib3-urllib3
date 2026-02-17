74047ec2e5221cfd2408a37d704278771934299f
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-17 18:19:21 +0800

    alvistack/2.6.3
        git clean -xdf
        tar zcvf ../python-urllib3_2.6.3.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-urllib3.spec ../python-urllib3_2.6.3-1.spec
        cp ../python*-urllib3*2.6.3*.{gz,xz,spec,dsc} /osc/home\:alvistack/urllib3-urllib3-2.6.3/
        rm -rf ../python*-urllib3_2.6.3*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+setup(
+    name='urllib3',
+    version='2.6.3',
+    description='HTTP library with thread-safe connection pooling, file post, and more.',
+    author_email='Andrey Petrov <andrey.petrov@shazow.net>',
+    maintainer_email='Seth Michael Larson <sethmichaellarson@gmail.com>, Quentin Pradet <quentin@pradet.me>, Illia Volochii <illia.volochii@gmail.com>',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Free Threading :: 2 - Beta',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+    ],
+    extras_require={
+        'brotli': [
+            'brotli>=1.2.0; platform_python_implementation == "CPython"',
+            'brotlicffi>=1.2.0.0; platform_python_implementation != "CPython"',
+        ],
+        'h2': [
+            'h2<5,>=4',
+        ],
+        'socks': [
+            'pysocks!=1.5.7,<2.0,>=1.5.6',
+        ],
+        'zstd': [
+            'backports-zstd>=1.0.0; python_version < "3.14"',
+        ],
+    },
+    packages=[
+        'urllib3',
+        'urllib3.contrib',
+        'urllib3.contrib.emscripten',
+        'urllib3.http2',
+        'urllib3.util',
+    ],
+    package_dir={'': 'src'},
+)
